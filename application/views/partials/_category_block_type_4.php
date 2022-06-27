4ad6d6b81b44d9ac148f47ae899f392d0fd00bd9
Aavash Dhimal <aavashdhimal9@gmail.com>
2022-06-27 10:27:27 +0545

    borders

--- application/views/partials/_category_block_type_4.php
+++ application/views/partials/_category_block_type_4.php
@@ -139,10 +139,6 @@
         spaceBetween: 30,
         slidesPerView: 3,
         centeredSlides: true,
-        autoplay: {
-          delay: 2500,
-          disableOnInteraction: false,
-        },
         pagination: {
           el: ".swiper-pagination",
           clickable: true,
